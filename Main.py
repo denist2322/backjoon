53bd3542b86a8c282429e33b4d5cd8688851ebb6
denist2322 <parksomang125@gmail.com>
2022-06-06 23:11:04 +0900

    별찍기 해결

--- Main.py
+++ Main.py
@@ -1,24 +1,26 @@
 n = int(input())
 
-result = ""
-
 
 def draw_star(n):
-    global result
     # n이 1일때 = n이 더이상 3으로 나눠질 수 없을 때
     if n == 1:
         return ["*"]
     # n이 1이 될때 까지 실행
     stars = draw_star(n//3)
-
+    print("stars :", n, stars)
+    result = []
+    # 3n + 1번째 줄
     for first in stars:
-        result += first*3
-
+        result.append(first*3)
+    # 3n + 2번째 줄
     for second in stars:
-        result += second + " "*(n//3) + second
-
+        result.append(second + " "*(n//3) + second)
+    # 3n + 3번째 줄
     for third in stars:
-        result += third*3
+        result.append(third*3)
+    print("result:", n, result)
+    return result
 
 
-print(draw_star(n))
+draw_star(n)
+# print("\n".join(draw_star(n)))
